442eff1b42f8ac23592f296ca9cec70808e31542
Alex <alexandre.morize@hotmail.fr>
2023-10-17 15:26:32 +0200

    v0.1

---
+++ Assets/PoolTester.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SR.ObjectPooler;
+
+public class PoolTester : MonoBehaviour
+{
+    [SerializeField] private GameObject obj;
+
+    [SerializeField] private bool instantiateOne;    
+    [SerializeField] private bool destroyLast;    
+    [SerializeField] private int population = 1;    
+    [SerializeField] private bool populate;
+
+    private GameObject Last;
+
+    private void InstOne()
+    {
+        if (!instantiateOne) return;
+        instantiateOne = false;
+        Last = ObjectPooler.InstantiateFromPool(obj);
+    }
+    private void DestroyLast()
+    {
+        if (!destroyLast) return;
+        destroyLast = false;
+        ObjectPooler.StockToPool(Last);
+    }
+
+    private void Populate()
+    {
+        if (!populate) return;
+        populate = false;
+        ObjectPooler.PopulatePool(obj,population);
+    }
+
+    private void FixedUpdate()
+    {
+        Populate();
+        DestroyLast();
+        InstOne();
+    }
+}
